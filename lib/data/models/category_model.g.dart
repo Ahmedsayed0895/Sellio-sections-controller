7a8fb6dcf0aa29f8c77a24c107bd0fcd9729d3c6
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-02-10 12:08:28 +0200

    Generate CategoryModel JSON serialization

---
+++ lib/data/models/category_model.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'category_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CategoryModel _$CategoryModelFromJson(Map<String, dynamic> json) =>
+    CategoryModel(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      subCategories: CategoryModel._subsFromJson(json['subCategories'] as List),
+    );
+
+Map<String, dynamic> _$CategoryModelToJson(CategoryModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'subCategories': CategoryModel._subsToJson(instance.subCategories),
+    };
+
+SubCategoryModel _$SubCategoryModelFromJson(Map<String, dynamic> json) =>
+    SubCategoryModel(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      categoryId: json['categoryId'] as String,
+    );
+
+Map<String, dynamic> _$SubCategoryModelToJson(SubCategoryModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'categoryId': instance.categoryId,
+    };
