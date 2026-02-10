ce16a1fb81a6d55efcbbec03676e6ee4ba27712d
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-02-10 14:37:56 +0200

    Update ISectionRepository interface

--- lib/domain/repositories/section_repository.dart
+++ lib/domain/repositories/section_repository.dart
@@ -2,7 +2,7 @@ import '../entities/section.dart';
 
 abstract class ISectionRepository {
   Future<List<CategorySection>> getSections();
-  Future<CategorySection> createSection(CategorySection section);
+  Future<List<CategorySection>> createSection(CategorySection section);
   Future<void> updateSection(String id, Map<String, dynamic> updates);
   Future<void> deleteSection(String id);
 }
