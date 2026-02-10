e3dcd91108a9a2e699ee5b3e801faba25935c722
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-02-10 14:37:55 +0200

    Update IRemoteDataSource interface

--- lib/data/datasources/remote_datasource.dart
+++ lib/data/datasources/remote_datasource.dart
@@ -3,7 +3,7 @@ import '../models/section_model.dart';
 
 abstract class IRemoteDataSource {
   Future<List<SectionModel>> fetchSections();
-  Future<SectionModel> createSection(SectionModel section);
+  Future<List<SectionModel>> createSection(SectionModel section);
   Future<void> updateSection(String id, Map<String, dynamic> updates);
   Future<void> deleteSection(String id);
   Future<List<CategoryModel>> fetchCategories();
