8893013ec05a0c986a3ef8a0d970f67bc03dcf37
Ahmed Sayed <ahmedsayed0895@gmail.com>
2026-02-10 14:37:48 +0200

    Refactor CategoryModel to standalone class

--- lib/data/models/category_model.dart
+++ lib/data/models/category_model.dart
@@ -1,54 +1,49 @@
 import 'package:json_annotation/json_annotation.dart';
-import '../../domain/entities/category.dart';
 
 part 'category_model.g.dart';
 
 @JsonSerializable()
-class CategoryModel extends Category {
-  @override
+class CategoryModel {
+  final String id;
+  final String title;
+
   @JsonKey(fromJson: _subsFromJson, toJson: _subsToJson)
-  final List<SubCategory> subCategories;
+  final List<SubCategoryModel> subCategories;
 
   const CategoryModel({
-    required super.id,
-    required super.title,
+    required this.id,
+    required this.title,
     required this.subCategories,
-  }) : super(subCategories: subCategories);
+  });
 
   factory CategoryModel.fromJson(Map<String, dynamic> json) =>
       _$CategoryModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$CategoryModelToJson(this);
 
-  static List<SubCategory> _subsFromJson(List<dynamic> json) {
+  static List<SubCategoryModel> _subsFromJson(List<dynamic> json) {
     return json
         .map((e) => SubCategoryModel.fromJson(e as Map<String, dynamic>))
         .toList();
   }
 
-  static List<Map<String, dynamic>> _subsToJson(List<SubCategory> subs) {
-    return subs
-        .map(
-          (e) => (e is SubCategoryModel)
-              ? e.toJson()
-              : SubCategoryModel(
-                  id: e.id,
-                  title: e.title,
-                  imageUrl: e.imageUrl,
-                  categoryId: e.categoryId,
-                ).toJson(),
-        )
-        .toList();
+  static List<Map<String, dynamic>> _subsToJson(List<SubCategoryModel> subs) {
+    return subs.map((e) => e.toJson()).toList();
   }
 }
 
 @JsonSerializable()
-class SubCategoryModel extends SubCategory {
+class SubCategoryModel {
+  final String id;
+  final String title;
+  final String? imageUrl;
+  final String categoryId;
+
   const SubCategoryModel({
-    required super.id,
-    required super.title,
-    super.imageUrl,
-    required super.categoryId,
+    required this.id,
+    required this.title,
+    this.imageUrl,
+    required this.categoryId,
   });
 
   factory SubCategoryModel.fromJson(Map<String, dynamic> json) =>
